7c08e40766e8d0c30ff923d805437c3f842cafca
ColinsGitCode <xueguodong2016@gmail.com>
2023-12-07 14:06:22 +0900

    project start-up

---
+++ AlgorithmsCoding/Links_Table/leetcode_N21.py
@@ -0,0 +1,17 @@
+# leetcode training
+# https://leetcode.cn/problems/merge-two-sorted-lists/
+
+# Definition for singly-linked list.
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+class LinkList:
+    def __init__(self):
+        pass
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+
+if __name__ == '__main__':
+    pass
